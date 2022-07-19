4c0cbe0af7bcf6234ad448f6802462d76515e292
Yezid Buriticá Luna <blak1997@gmail.com>
2022-07-18 21:54:39 -0500

    ejercicio_1

---
+++ src/com/ejercicio/TiposDatos.java
@@ -0,0 +1,54 @@
+package com.ejercicio;
+
+public class TiposDatos {
+
+	public static void main( String[] args) {
+	  
+		// Tipos de datos
+		
+		// Numericos
+		
+		// Enteros
+		
+		byte varByte = 5;
+		short varShort = 20;
+		int varInt = 50;
+		long varLong = 1000;
+		
+		// decimales
+		float varFloat = 5.5f; 
+		double varDouble = 4.4d;
+		
+		// booleano
+		boolean varBooleanTrue = true;
+		boolean varBooleanFalse = false;
+		
+		// texto
+		String varString = "Mi variable";
+		String varChar = "C";
+		
+		// Impresion
+		System.out.println("Tipos de datos");
+		System.out.println("---------------");
+		System.out.println("1. Numericos");
+		System.out.println("1.1 Enteros \n");
+		System.out.println(varByte);
+		System.out.println(varShort);
+		System.out.println(varInt);
+		System.out.println(varLong);
+		System.out.println("---------------");
+		System.out.println("1.2 Decimales  \n");
+		System.out.println(varFloat);
+		System.out.println(varDouble);
+		System.out.println("---------------");		
+		System.out.println("2. Booleanos  \n");
+		System.out.println(varBooleanFalse);
+		System.out.println(varBooleanTrue);
+		System.out.println("---------------");	
+		System.out.println("3. Texto  \n");		
+		System.out.println(varChar);
+		System.out.println(varString);
+		
+		
+	} 
+}
